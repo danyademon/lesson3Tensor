a69d59d1b73443da6a7eb1e3ec79b7c33738a7a8
danyademon <119026943+danyademon@users.noreply.github.com>
2022-12-02 08:55:20 +0500

    Add files via upload

--- task3.py
+++ task3.py
@@ -1,24 +1,28 @@
 import math
-import sys
 
-try:
-    a = float(input("Type a: "))
-    b = float(input("Type b: "))
-    c = float(input("Type c: "))
-except:
-    print('Invalid input format. Coefficients cant be string, bool and etc.')
-    sys.exit()
+while True:
+    try:
+        a = float(input("Type a: "))
+        b = float(input("Type b: "))
+        c = float(input("Type c: "))
+    except:
+        print('Invalid input format. Coefficients cant be string, bool and etc. Try again.')
+        temp = input('Type "0" for exit. Else continue.')
+        if temp == '0':
+            break
+        else: continue
 
-output = f'Your quadratic equation: {a}x^2+{b}x+{c}=0' 
-print (output.replace('+-', '-'))
+    output = f'Your quadratic equation: {a}x^2+{b}x+{c}=0' 
+    print (output.replace('+-', '-'))
 
-D = b*b-4*a*c
-print(f'Discriminant: {D}')
+    D = b*b-4*a*c
+    print(f'Discriminant: {D}')
 
-if D>0:
-    print(f'x1 = {(-b+math.sqrt(D))/(2*a)} ')
-    print(f'x2 = {(-b-math.sqrt(D))/(2*a)} ')
-elif D==0:
-    print(f'x1 = {-b/(2*a)} ')
-elif D<0:
-    print('No solutions')
+    if D>0:
+        print(f'x1 = {(-b+math.sqrt(D))/(2*a)} ')
+        print(f'x2 = {(-b-math.sqrt(D))/(2*a)} ')
+    elif D==0:
+        print(f'x1 = {-b/(2*a)} ')
+    elif D<0:
+        print('No solutions')
+    break
